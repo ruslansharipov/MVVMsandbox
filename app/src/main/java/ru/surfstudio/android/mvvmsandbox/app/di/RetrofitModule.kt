bad6b1484c4696f4428aa32449cb8a1fbf77820a
Ruslan Sharipov <sharipov@surfstudio.ru>
2021-03-10 16:28:07 +0300

    логирование запросов и переезд на моковый сервер

--- app/src/main/java/ru/surfstudio/android/mvvmsandbox/app/di/RetrofitModule.kt
+++ app/src/main/java/ru/surfstudio/android/mvvmsandbox/app/di/RetrofitModule.kt
@@ -3,6 +3,7 @@ package ru.surfstudio.android.mvvmsandbox.app.di
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -12,11 +13,21 @@ class RetrofitModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://upstage.rivegauche.ru/rg/v1/newRG/")
-            .client(OkHttpClient())
+            .baseUrl("https://r2.mocker.surfstudio.ru/")
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesOkhttp() : OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient.Builder()
+                .addNetworkInterceptor(interceptor)
+                .build()
     }
 }
