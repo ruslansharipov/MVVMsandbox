b70a8c62691e622f336af3bbdcf0e8d06697c350
Ruslan Sharipov <sharipov@surfstudio.ru>
2021-03-12 11:20:26 +0300

    кнопка повторения запроса для экрана с пагинацией

--- app/src/main/java/ru/surfstudio/android/mvvmsandbox/feature/products/ProductsFragmentView.kt
+++ app/src/main/java/ru/surfstudio/android/mvvmsandbox/feature/products/ProductsFragmentView.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.view.isVisible
@@ -20,6 +22,8 @@ class ProductsFragmentView : Fragment() {
     @Inject
     lateinit var viewModel: IProductsViewModel
 
+    private lateinit var errorContainer: LinearLayout
+    private lateinit var retryBtn: Button
     private lateinit var errorTv: TextView
     private lateinit var loadingPb: ProgressBar
     private lateinit var productsRv: RecyclerView
@@ -50,10 +54,14 @@ class ProductsFragmentView : Fragment() {
     private fun initViews(view: View) {
         loadingPb = view.findViewById(R.id.products_pb)
         errorTv = view.findViewById(R.id.products_error_tv)
+        retryBtn = view.findViewById(R.id.products_retry_btn)
+        errorContainer = view.findViewById(R.id.products_error_container)
 
         productsRv = view.findViewById<RecyclerView>(R.id.products_rv)
         productsRv.layoutManager = GridLayoutManager(requireContext(), 2)
         productsRv.adapter = easyAdapter
+
+        retryBtn.setOnClickListener { viewModel.loadMode() }
     }
 
     private fun bindViews() {
@@ -64,7 +72,7 @@ class ProductsFragmentView : Fragment() {
             val hasData = requestUi.data?.hasData ?: false
             productsRv.isVisible = hasData
             loadingPb.isVisible = !hasData && requestUi.isLoading
-            errorTv.isVisible = !hasData && !requestUi.isLoading && requestUi.hasError
+            errorContainer.isVisible = !hasData && !requestUi.isLoading && requestUi.hasError
 
             errorTv.text = requestUi.error?.message
         }
