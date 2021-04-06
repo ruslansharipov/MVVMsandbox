6490393adee475ead615d5bee9129be5f60a3511
Ruslan Sharipov <sharipov@surfstudio.ru>
2021-04-06 12:37:26 +0300

    правка экстеншена на более идиоматичный

--- app/src/main/java/ru/surfstudio/android/mvvmsandbox/util/RequestUtil.kt
+++ app/src/main/java/ru/surfstudio/android/mvvmsandbox/util/RequestUtil.kt
@@ -3,13 +3,14 @@ package ru.surfstudio.android.mvvmsandbox.util
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
 import ru.surfstudio.android.core.mvp.binding.rx.request.Request
 
 suspend fun <T> request(
-    dispatcher: CoroutineDispatcher = Dispatchers.IO,
-    requestFunc: suspend () -> T
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        requestFunc: suspend () -> T
 ): Request<T> {
     return withContext(dispatcher) {
         try {
@@ -21,12 +22,10 @@ suspend fun <T> request(
 }
 
 fun <T> requestFlow(requestFunc: suspend () -> T): Flow<Request<T>> {
-    return flow<Request<T>> {
+    return flow {
         emit(Request.Loading())
-        try {
-            emit(Request.Success(requestFunc()))
-        } catch (e: Exception) {
-            emit(Request.Error(e))
-        }
+        emit(Request.Success(requestFunc()))
+    }.catch { error ->
+        emit(Request.Error(error))
     }
 }
