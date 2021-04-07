ede151ac86576c21f33b22563a1ca0a7afa76f06
Ruslan Sharipov <sharipov@surfstudio.ru>
2021-04-07 10:46:30 +0300

    добавлены комменты к утилитарным методам

--- app/src/main/java/ru/surfstudio/android/mvvmsandbox/util/RequestUtil.kt
+++ app/src/main/java/ru/surfstudio/android/mvvmsandbox/util/RequestUtil.kt
@@ -5,9 +5,14 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.withContext
 import ru.surfstudio.android.core.mvp.binding.rx.request.Request
 
+/**
+ * Обертка над запросом в сеть, возвращающая данные в виде [Request.Success] в случае успеха и
+ * ошибку в [Request.Error], если в ходе запроса произошла ошибка
+ */
 suspend fun <T> request(
         dispatcher: CoroutineDispatcher = Dispatchers.IO,
         requestFunc: suspend () -> T
@@ -21,10 +26,16 @@ suspend fun <T> request(
     }
 }
 
+/**
+ * Обертка над Flow, которая эмитит [Request.Loading] до того как основной Flow начнет свои эмиты,
+ * Ошибка, полученная в ходе запроса оборачивается в [Request.Error].
+ * Успешно загруженные данные приходят в виде [Request.Success]
+ */
 fun <T> requestFlow(requestFunc: suspend () -> T): Flow<Request<T>> {
-    return flow {
-        emit(Request.Loading())
+    return flow<Request<T>> {
         emit(Request.Success(requestFunc()))
+    }.onStart {
+        emit(Request.Loading())
     }.catch { error ->
         emit(Request.Error(error))
     }
