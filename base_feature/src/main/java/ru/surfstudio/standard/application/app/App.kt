d366c10154cf207f5fd7b34a7882f6219a56b5d0
Ruslan Sharipov <sharipov@surfstudio.ru>
2021-04-14 11:46:17 +0300

    установка анимаций переходов по-умолчанию

--- base_feature/src/main/java/ru/surfstudio/standard/application/app/App.kt
+++ base_feature/src/main/java/ru/surfstudio/standard/application/app/App.kt
@@ -2,11 +2,14 @@ package ru.surfstudio.standard.application.app
 
 import android.app.Application
 import ru.surfstudio.android.logger.Logger
+import ru.surfstudio.android.navigation.animation.DefaultAnimations
+import ru.surfstudio.android.navigation.animation.resource.EmptyResourceAnimations
 import ru.surfstudio.android.navigation.provider.callbacks.ActivityNavigationProviderCallbacks
 import ru.surfstudio.standard.application.app.di.AppComponent
 import ru.surfstudio.standard.application.app.di.AppModule
 import ru.surfstudio.standard.application.app.di.DaggerAppComponent
 import ru.surfstudio.standard.application.logger.strategies.local.TimberLoggingStrategy
+import ru.surfstudio.standard.ui.animation.SlideAnimations
 
 class App : Application() {
 
@@ -22,6 +25,7 @@ class App : Application() {
         initLogCallbacks()
         initDiCallbacks()
         initNavigationCallbacks()
+        initDefaultAnimations()
     }
 
     private fun initLogCallbacks() {
@@ -40,5 +44,11 @@ class App : Application() {
 
     private fun initLogger() {
         Logger.addLoggingStrategy(TimberLoggingStrategy())
+    }
+
+    private fun initDefaultAnimations() {
+        DefaultAnimations.fragment = SlideAnimations
+        DefaultAnimations.activity = SlideAnimations
+        DefaultAnimations.tab = EmptyResourceAnimations
     }
 }
